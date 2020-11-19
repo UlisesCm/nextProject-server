0d608a198044a74ed74cf075e54209fe3cfa27ea
Ulises Cipŕes <ulisescipresm@gmail.com>
2020-11-19 14:41:12 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8a8a68226f295e2fb62e969c23f354e5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '09197d5a-61d6-4927-a43a-7d69ca2457a2'
+};
